168784b7f62248f616e584171029fcd20fbbed6e
Irina Ogereleva <irinaogereleva@gmail.com>
2014-10-08 15:55:50 +0400

    add popular goods slider

--- js/script.js
+++ js/script.js
@@ -13,5 +13,15 @@ $(function(){
 		autoHover: true,
 		pause: 2000
 	});
+
+	$('.bxslider-3').bxSlider({
+		maxSlides: 5,
+		slideWidth: 146,
+		slideMargin: 17,
+		moveSlides: 1,
+		// auto: true,
+		autoHover: true,
+		pause: 2000
+	});
 	
 });
