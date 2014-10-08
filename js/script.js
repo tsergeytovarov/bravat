09a9d4ce3b9dfe7403a464dd519134a48e22428d
Irina Ogereleva <irinaogereleva@gmail.com>
2014-10-08 15:30:16 +0400

    add design-catalog products

--- js/script.js
+++ js/script.js
@@ -3,5 +3,15 @@ $(function(){
 	$('.bxslider').bxSlider({
 		auto: true,
 	});
+
+	$('.bxslider-2').bxSlider({
+		maxSlides: 5,
+		slideWidth: 146,
+		slideMargin: 17,
+		moveSlides: 1,
+		auto: true,
+		autoHover: true,
+		pause: 2000
+	});
 	
 });
